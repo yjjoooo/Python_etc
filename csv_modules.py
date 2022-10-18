2427d40e2210459d82607262db4f24297c709dc5
yjjoooo <whss9173@naver.com>
2022-10-18 22:08:42 +0900

    update logging, comment

--- csv_modules.py
+++ csv_modules.py
@@ -16,60 +16,62 @@ def log_err(msg):
 # csv reader (UP plz!)
 def read_csv(path):
     try:
-        log('#### Read CSV {}'.format(path))
+        log('#### Read CSV \"{}\"'.format(path))
         return pd.read_csv(path)
     except RuntimeWarning as w:
         log_warn(w)
     except:
-        log_err('############ Read CSV {} Error'.format(path))
+        log_err('############ Read CSV \"{}\" Error'.format(path))
         log(traceback.format_exc())
 
 # csv saver
 def save_csv(df, path):
     try:
-        log('#### Save CSV {}'.format(path))
+        log('#### Save CSV \"{}\"'.format(path))
         
         file_name, extension = os.path.splitext(path)
         
         save_count = 0
         while True:
             if os.path.exists(path):
+                log('######## CSV \"{}\" Already Exist'.format(path))
                 save_count += 1
                 path = '{}_{}{}'.format(file_name, save_count, extension)
             else:
                 break
             
+        log('######## Save CSV \"{}\" as \"{}\"'.format(file_name + extension, path))
         df.to_csv(path, encoding = 'UTF-8', index = False)
     except RuntimeWarning as w:
         log_warn(w)
     except:
-        log_err('############ Save CSV {} Error'.format(path))
+        log_err('############ Save CSV \"{}\" Error'.format(path))
         log(traceback.format_exc())
 
 # big csv reader
 def read_big_csv(path, chunksize):
     try:
-        log('#### Read CSV {}'.format(path))
+        log('#### Read CSV \"{}\"'.format(path))
         return pd.read_csv(path, chunksize = chunksize)
     except RuntimeWarning as w:
         log_warn(w)
     except:
-        log_err('############ Read CSV {} Error'.format(path))
+        log_err('############ Read CSV \"{}\" Error'.format(path))
         log(traceback.format_exc())
 
 # big csv saver
 def save_big_csv(df, path, index):
     try:
-        log('#### Save CSV {}'.format(path))
+        log('#### Save CSV {0: >3} {}'.format('({})'.format(index), path))
         
         if index == 0:
             df.to_csv(path, encoding = 'UTF-8', index = False)
         elif index > 0:
             df.to_csv(path, encoding = 'UTF-8', index = False, header = False, mode = 'a')
         else:
-            log_err('############ Save CSV {} Index Error'.format(path))
+            log_err('######## Please Check CSV Index \"{}\"'.format(path))
     except RuntimeWarning as w:
         log_warn(w)
     except:
-        log_err('############ Save CSV {} Error'.format(path))
+        log('############ Save CSV {0: >3} \"{}\" Error'.format('({})'.format(index), path))
         log(traceback.format_exc())
