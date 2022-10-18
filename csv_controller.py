28e17890ea9063467fd37b04ed50d6333e991e36
yjjoooo <whss9173@naver.com>
2022-10-18 21:49:06 +0900

    create csv controller

---
+++ csv_controller.py
@@ -0,0 +1,75 @@
+import logging
+import traceback
+import pandas as pd
+import os
+
+# logger
+def log(msg):
+    logging.info(msg)
+
+def log_warn(msg):
+    logging.warning(msg)
+
+def log_err(msg):
+    logging.error(msg)
+
+# csv reader (UP plz!)
+def read_csv(path):
+    try:
+        log('#### Read CSV {}'.format(path))
+        return pd.read_csv(path)
+    except RuntimeWarning as w:
+        log_warn(w)
+    except:
+        log_err('############ Read CSV {} Error'.format(path))
+        log(traceback.format_exc())
+
+# csv saver
+def save_csv(df, path):
+    try:
+        log('#### Save CSV {}'.format(path))
+        
+        file_name, extension = os.path.splitext(path)
+        
+        save_count = 0
+        while True:
+            if os.path.exists(path):
+                save_count += 1
+                path = '{}_{}{}'.format(file_name, save_count, extension)
+            else:
+                break
+            
+        df.to_csv(path, encoding = 'UTF-8', index = False)
+    except RuntimeWarning as w:
+        log_warn(w)
+    except:
+        log_err('############ Save CSV {} Error'.format(path))
+        log(traceback.format_exc())
+
+# big csv reader
+def read_big_csv(path, chunksize):
+    try:
+        log('#### Read CSV {}'.format(path))
+        return pd.read_csv(path, chunksize = chunksize)
+    except RuntimeWarning as w:
+        log_warn(w)
+    except:
+        log_err('############ Read CSV {} Error'.format(path))
+        log(traceback.format_exc())
+
+# big csv saver
+def save_big_csv(df, path, index):
+    try:
+        log('#### Save CSV {}'.format(path))
+        
+        if index == 0:
+            df.to_csv(path, encoding = 'UTF-8', index = False)
+        elif index > 0:
+            df.to_csv(path, encoding = 'UTF-8', index = False, header = False, mode = 'a')
+        else:
+            log_err('############ Save CSV {} Index Error'.format(path))
+    except RuntimeWarning as w:
+        log_warn(w)
+    except:
+        log_err('############ Save CSV {} Error'.format(path))
+        log(traceback.format_exc())
